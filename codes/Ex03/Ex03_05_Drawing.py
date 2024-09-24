2362b3778cc2ac74084d880d50d1baaff22d7682
Dknife <young.min.kang@gmail.com>
2024-09-24 12:38:33 +0900

    Add files via upload

--- codes/Ex03/Ex03_05_Drawing.py
+++ codes/Ex03/Ex03_05_Drawing.py
@@ -5,8 +5,22 @@ from PyQt6.QtWidgets import QApplication, QWidget
 from PyQt6.QtOpenGLWidgets import QOpenGLWidget
 
 import sys
-import random
+import math
 
+def draw_circle(cx, cy, r) :
+    # 원그리기
+    angle = 0    
+    n_points = 100
+    d_angle = 2 * 3.141592 / n_points # radian angle interval
+    glColor3f(1, 1, 1)
+    glBegin(GL_POLYGON)
+    for i in range(n_points):
+        x, y = r * math.cos(angle) + cx , r * math.sin(angle) + cy
+        glVertex2f(x, y)
+        angle += d_angle
+        
+    glEnd()
+    
 class MyGLWindow(QOpenGLWidget):
     def __init__(self, argv):
         super().__init__()
@@ -26,18 +40,7 @@ class MyGLWindow(QOpenGLWidget):
     def paintGL(self):
         glClear(GL_COLOR_BUFFER_BIT)
         
-        glColor3f(0.2, 0.2, 0.2)
-        r = [-0.9, 0.9]
-        glBegin(GL_POINTS)
-        for i in range(10000):
-            x = random.randint(0, 100)
-            y = random.randint(0, 100)   
-            interval = r[1] - r[0] 
-            x = interval * x / 100 + r[0]
-            y = interval * y / 100 + r[0]
-            glVertex2f(x, y)
-        glEnd()
-         
+    
         # 좌표 축을 그려보자
         # x 축은 붉은 색
         # y 축은 초록 색
@@ -50,6 +53,20 @@ class MyGLWindow(QOpenGLWidget):
         glVertex2f(0, 1)
         glVertex2f(0, -1)
         glEnd()
+        
+        # 삼각형 그리기
+        glBegin(GL_TRIANGLES)
+        glColor3f(0, 0.5, 0)
+        glVertex2f(-0.75, 0)
+        glVertex2f(0.25, 0)
+        glVertex2f(-0.25, 0.5)
+        glEnd()
+        
+        draw_circle(0.5, 0.5, 0.25)
+        draw_circle(0.25, 0.25, 0.15)
+        
+        
+        
         
         
         
