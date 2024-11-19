4469a0bf4a365e64ab283e1787b8ca3c27ae5c45
Dknife <young.min.kang@gmail.com>
2024-11-19 11:48:07 +0900

    Create Ex0704_Transform.py

---
+++ codes/07_Transform/Ex0704_Transform.py
@@ -0,0 +1,79 @@
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+from PyQt6.QtWidgets import *
+from PyQt6.QtOpenGLWidgets import *
+from PyQt6.QtCore import *
+
+import sys
+import numpy as np
+import math
+import Drawable
+import MeshLoader
+           
+      
+       
+class MyGLWindow(QOpenGLWidget) :
+    def __init__(self):
+        super().__init__()
+        self.myMesh = MeshLoader.MeshLoader()
+        self.axisDraw = Drawable.Drawable()  
+    
+    def initializeGL(self):
+        glClearColor(0.5, 0.5, 1.0, 1.0)
+        glEnable(GL_DEPTH_TEST)
+        self.myMesh.loadMesh('cow.txt')
+        self.myMesh.prepareBufferRendering()
+    
+    def resizeGL(self, w, h):
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        gluPerspective(60, w/h, 0.1, 1000)
+    
+    def paintGL(self):
+        
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+        
+        gluLookAt(2, 2, 5, 0, 0, 0, 0, 1, 0)
+        
+        # draw code here        
+        self.myMesh.render()
+        self.axisDraw.draw()
+        
+        # 한 마리 더 추가
+        glTranslatef(2, 0, 0)
+        self.myMesh.render()
+        self.axisDraw.draw()
+        
+        # 한 마리 더 추가
+        glRotatef(180, 0, 1, 0)
+        glTranslatef(0, 2, 0)
+        self.myMesh.render()
+        self.axisDraw.draw()
+        
+        # 한 마리 더 추가
+        glTranslatef(2, 0, 0)
+        self.myMesh.render()
+        self.axisDraw.draw()
+               
+class MainWindow(QMainWindow) :
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.glWidget = MyGLWindow()
+        self.setCentralWidget(self.glWidget)
+        
+
+
+        
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    app.exec()
+    
+main()
+                         
+        
+        
