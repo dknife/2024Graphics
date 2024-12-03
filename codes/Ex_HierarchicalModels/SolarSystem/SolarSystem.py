c943b5d1c0d5d34dbd3f22dea72e3c2067e5922c
Dknife <young.min.kang@gmail.com>
2024-12-03 10:27:07 +0900

    Add files via upload

--- codes/Ex_HierarchicalModels/SolarSystem/SolarSystem.py
+++ codes/Ex_HierarchicalModels/SolarSystem/SolarSystem.py
@@ -9,10 +9,13 @@ from PyQt6.QtCore import *
 import sys
 import numpy as np
 import math
+import Sphere
       
 class MyGLWindow(QOpenGLWidget) :
     def __init__(self):
         super().__init__()
+        self.mySphere = Sphere.Sphere()
+        self.sun_angle = 0.0
     
     def initializeGL(self):
         glClearColor(0.5, 0.5, 1.0, 1.0)
@@ -31,6 +34,15 @@ class MyGLWindow(QOpenGLWidget) :
         
         gluLookAt(3, 3, 15, 0, 0, 0, 0, 1, 0)
         
+        glRotatef(self.sun_angle, 0, 1, 0)
+        self.mySphere.draw()
+        
+    def rotateSun(self):
+        self.sun_angle += 1.0
+        if self.sun_angle >= 360 :
+            self.sun_angle -= 360            
+        self.update()
+        
         
         
            
@@ -39,6 +51,15 @@ class MainWindow(QMainWindow) :
         QMainWindow.__init__(self)
         self.glWidget = MyGLWindow()
         self.setCentralWidget(self.glWidget)
+        
+        self.Timer = QTimer()
+        self.Timer.setInterval(1)
+        self.Timer.timeout.connect(self.timeout)
+        self.Timer.start()
+        
+    def timeout(self):
+        self.glWidget.rotateSun()
+        
 
 def main():
     app = QApplication(sys.argv)
