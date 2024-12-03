da6f901df46f2d76338fe4faf44869e06b1aaf29
Dknife <young.min.kang@gmail.com>
2024-12-03 11:21:58 +0900

    Add files via upload

--- codes/Ex_HierarchicalModels/SolarSystem/SolarSystem.py
+++ codes/Ex_HierarchicalModels/SolarSystem/SolarSystem.py
@@ -18,6 +18,13 @@ class MyGLWindow(QOpenGLWidget) :
         self.sun_angle = 0.0
         self.earth_revolve = 0.0 # 공전
         self.earth_rotate = 0.0 # 회전
+        self.moon_revolve = 0.0 # 달의 공전
+        self.jupiter_revolve = 0.0
+        self.jupiter_rotate = 0.0
+        self.jupiter_moon1_revolve = 0.0
+        self.jupiter_moon1_rotate = 0.0
+        self.jupiter_moon2_revolve = 0.0
+        self.jupiter_moon2_rotate = 0.0
     
     def initializeGL(self):
         glClearColor(0.5, 0.5, 1.0, 1.0)
@@ -41,16 +48,79 @@ class MyGLWindow(QOpenGLWidget) :
         self.mySphere.draw() # SUN
         glPopMatrix() #### 태양 자전 변환의 끝
         
+        ################# 지구계를 고립시키자
+        glPushMatrix()
+        
         glRotatef(self.earth_revolve, 0, 1, 0) ## 공전
         glTranslatef(3, 0, 0)
+        glPushMatrix() ############ 지구 자전을 고립시키기 ###########
         glRotatef(self.earth_rotate, 0, 1, 0) ## 자전
+        #####################################
+        # earth
+        glPushMatrix()
         glScalef(0.3, 0.3, 0.3)
         self.mySphere.draw() # EARTH
+        glPopMatrix()
+        #####################################
+        glPopMatrix() ############ 지구 자전을 고립시키기 ###########
+        
+        ## MOON
+        glRotate(self.moon_revolve, 0.1, 1, 0.2)
+        glTranslatef(1, 0, 0)
+        glScalef(0.1, 0.1, 0.1)
+        self.mySphere.draw() # MOON
+        
+        glPopMatrix()
+        ################# 지구계를 고립시키자
+        
+        ############ 목성계 시작
+        glPushMatrix()
+        
+        glRotatef(self.jupiter_revolve, 0, 1, 0)
+        glTranslatef(5, 0, 0)
+        glPushMatrix() ######## 위성에 영향을 주면 안 되는 변환
+        glRotatef(self.jupiter_rotate, 0, 1, 0)
+        glScalef(0.5, 0.5, 0.5)
+        self.mySphere.draw()
+        glPopMatrix()  ######## 위성에 영향을 주면 안 되는 변환
+        
+        
+        ############### 위성 1 고립
+        glPushMatrix()
+        glRotatef(self.jupiter_moon1_revolve, 0, 1, 0)
+        glTranslatef(0.9, 0, 0)
+        glRotatef(self.jupiter_moon1_rotate, 0, 1, 0)
+        glScalef(0.2, 0.2, 0.2)
+        self.mySphere.draw()
+        glPopMatrix()
+        
+        ############### 위성 2
+        glPushMatrix()
+        glRotatef(self.jupiter_moon2_revolve, 0, 1, 0)
+        glTranslatef(1.5, 0, 0)
+        glRotatef(self.jupiter_moon2_rotate, 0, 1, 0)
+        glScalef(0.2, 0.2, 0.2)
+        self.mySphere.draw()
+        glPopMatrix()
+        
+        
+        glPopMatrix()
+        ################# 목성계 끝
+        
+        
         
     def increaseAngles(self):
         self.sun_angle = self.increaseAngle(self.sun_angle, 0.1)
         self.earth_revolve = self.increaseAngle(self.earth_revolve, 1.23)
-        self.earth_rotate = self.increaseAngle(self.earth_rotate, 3.1)
+        self.earth_rotate = self.increaseAngle(self.earth_rotate, 13.1)
+        self.moon_revolve = self.increaseAngle(self.moon_revolve, 2.0)
+        self.jupiter_revolve = self.increaseAngle(self.jupiter_revolve, 3.1)
+        self.jupiter_rotate = self.increaseAngle(self.jupiter_rotate, 3.1)
+        self.jupiter_moon1_revolve = self.increaseAngle(self.jupiter_moon1_revolve, 2.1)
+        self.jupiter_moon1_rotate = self.increaseAngle(self.jupiter_moon1_rotate, 5.1)
+        self.jupiter_moon2_revolve = self.increaseAngle(self.jupiter_moon2_revolve, 10.1)
+        self.jupiter_moon2_rotate = self.increaseAngle(self.jupiter_moon2_rotate, 13.1)
+        
         
     def increaseAngle(self, angle, step):
         angle += step
