b84f79ebceeeb79676c52877f2cd22acac93f8af
Dknife <young.min.kang@gmail.com>
2024-12-10 11:54:36 +0900

    Add files via upload

--- codes/Ex08_NormalsAndLights/drawPlane.py
+++ codes/Ex08_NormalsAndLights/drawPlane.py
@@ -1,25 +1,36 @@
-
 from OpenGL.GL import *
 from OpenGL.GLU import *
 import numpy as np
 
+from PIL import Image
+# pip install Pillow 필요
+
+
 TEXSIZE = 16
 
 class TexturedPlane() :
-    def __init__(self) :
+    def __init__(self, file_name = "") :
         self.tex = None
-        myImage = self.create_texture_image()
+        if file_name == "":
+            myImage = self.create_texture_image()
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 
+                         TEXSIZE, TEXSIZE, 0, GL_RGB, GL_FLOAT, myImage)
+        else :
+            img = Image.open(file_name)
+            myImage = img.tobytes('raw', 'RGB', 0, -1)
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB,
+                        img.width, img.height, 0, GL_RGB, GL_UNSIGNED_BYTE, myImage)
+            
         self.setup_texture(myImage)
         
 
     def create_texture_image(self):
         return np.random.rand(TEXSIZE, TEXSIZE, 3)
 
-    def setup_texture(self, imageData):
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, TEXSIZE, TEXSIZE, 0, GL_RGB, GL_FLOAT, imageData)
+    def setup_texture(self, imageData):        
         glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
         glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
-        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
         glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
         
 
@@ -37,3 +48,4 @@ class TexturedPlane() :
         glTexCoord2d(2, 0)
         glVertex3f( 1, 0, -1)
         glEnd()
+        glDisable(GL_TEXTURE_2D)
